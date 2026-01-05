33eeb3812208d5e15a9ff81ad8bb2ffca41fa53d
marketadda <marketadda43@gmail.com>
2026-01-05 15:05:33 +0530

    Create basic Flutter app structure
    Initial implementation of a Flutter app with a main screen.

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Market Adda',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Market Adda')),
+      body: Center(child: Text('Welcome to Market Adda')),
+    );
+  }
+}
